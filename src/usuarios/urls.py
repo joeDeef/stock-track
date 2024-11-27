0a581f5c5cf5fde9f9fb85b2ef97821946b46817
Joel Defaz <105808520+JoelDefaz@users.noreply.github.com>
2024-11-27 17:52:56 -0500

    [Refactor] : Cambiar nombres de urls en los templates y views

--- src/usuarios/urls.py
+++ src/usuarios/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('login/', views.get_login, name='login'),
     path('signup/', views.get_signup, name='signup'),
     path('crearCuenta/', views.crear_cuenta, name='crearCuenta'),
-    path('IniciarSesion/', views.iniciar_sesion, name='IniciarSesion'),
-    path('logout/', LogoutView.as_view(next_page='inicio'), name='logout'),  # Redirigir al nombre de la URL 'inicio'
+    path('iniciarSesion/', views.iniciar_sesion, name='iniciarSesion'),
+    path('logout/', LogoutView.as_view(next_page='inicio'), name='logout'),
 ]
